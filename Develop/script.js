bc3e24d47352fe5a354aa4aaad9ca9fe0d70b3b8
MrPinkPanther10 <90013822+MrPinkPanther10@users.noreply.github.com>
2021-11-25 12:06:38 -0500

    final working product i think

--- Develop/script.js
+++ Develop/script.js
@@ -19,4 +19,46 @@ $(document).ready(function () {
         // Local Storage 
         localStorage.setItem(time, text);
     })
+
+    // getting value from local storage to show description
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    function currentTime() {
+        // tracks current hour
+        var currentHour = moment().hour();
+
+        // loop over time-blocks
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+            // check time and add classes 
+            if (timeBlock < currentHour) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (timeBlock === currentHour) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    currentTime();
 })
